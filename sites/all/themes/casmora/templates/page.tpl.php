0827fb0f433474c88a465d8328d3948a70c872ee
Sergio Vargas <sergio.svargass@gmail.com>
2013-11-04 15:28:13 -0500

    Commit normal

--- sites/all/themes/casmora/templates/page.tpl.php
+++ sites/all/themes/casmora/templates/page.tpl.php
@@ -1,18 +1,5 @@
 <?php
-// Theme to path
-global $theme_path;
-// Complete path to Drupal installation and theme
-$path = $base_path . $theme_path;
-
-/* Regions:
-* - $page['help']: Items for the header help.
-* - $page['navigation']: Items for the header navigation.
-* - $page['content']: Items for the header content.
-* - $page['superior']: Items for the header superior.
-* - $page['inferior']: Items for the header inferior.
-* - $page['right']: Items for the header right.
-* - $page['left']: Items for the header left.
-*
+/*
 * General utility variables:
 * - $base_path: The base URL path of the Drupal installation. At the very
 *   least, this will always default to /.
@@ -34,59 +21,26 @@ $path = $base_path . $theme_path;
 
 <!--CONTENEDOR-->
 <div class="contenedor">
-  <div class="header-tot">   
-    <div class="preheader-cont">        
-      <div class="preheader">     
-        <div class="preheader-de">   
-          <!--LOGO-->
-          <?php if ($logo): ?>
-            <div class="logo">
-              <a href="<?php print $base_path ?>" title="<?php print t('Home') ?>">
-                <img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" />
-              </a>
-            </div>
-          <?php endif ?>
-          <?php print render($page['header']); ?> 
-          <div class="clear"></div>
-        </div>
-        <div class="clear"></div>
+  <div class="region_header">   
+    <?php if ($logo): ?>
+      <div class="logo">
+        <a href="<?php print $base_path ?>" title="<?php print t('Home') ?>">
+          <img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" />
+        </a>
       </div>
-    </div> 
+    <?php endif ?>
+    <?php print render($page['header']); ?> 
   </div>
+  
+  <div class="region_content">
     <?php if ($messages): ?><div class="title" id="page-title"><?php print $messages; ?></div><?php endif; ?>
-  <div id="cs-slider">
-    <!--RHELP-->
-    <?php print render($page['slider']); ?> 
-  </div>
-
-</div>   
-<div class="home-cont">
-  <div class="home"> 
-    <div class="home-de"></div>
-    <div class="clear"></div>
     <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
     <?php print render($page['content']); ?>
+  </div>
 
+  <!--FOOTER-->
+  <div class="region_footer">
+    <?php print render($page['footer']); ?>
   </div>
-</div>
-
-<!--FOOTER-->
-<div class="footer-borde"></div>
-<div class="footer-cont">
-  <!--RINFERIOR-->
-  <?php print render($page['inferior']); ?>
-  <div class="footer">
-    <div class="footer-top">
-      <div class="fili"><img src="<?php print $path; ?>/images/fili.png" /></div>
-      <?php print render($page['footer']); ?>
-      <div class="footer-logo"><img src="<?php print $path; ?>/images/logo-footer.png" /></div>
-      <div class="clear"></div>  
-  </div>
-  <div class="postfooter-cont">
-    <div class="postfooter">
-      <div class="footer-linea"></div>
-      <?php print render($page['postfooter']); ?>
-    </div>
-  </div>
-</div>
+</div>   
